e3381a9ef7473cdbb0c124a343ef66558f154b22
qijidewo1991 <297452521@qq.com>
2018-05-23 14:09:04 +0800

    修改路单送达时间 和 开始配送时间问题 发现电子路单对接有问题，发车时间 和创建运单时间 填错了，DespatchActualDateTime 发车时间写成了送达时间ConsignmentDateTime创建运单时间写成了发车时间

--- xframe-hyb/src/com/hyb/utils/DataSender.java
+++ xframe-hyb/src/com/hyb/utils/DataSender.java
@@ -60,8 +60,8 @@ public class DataSender {
 			data.put("Carrier", "河南省脱颖实业有限公司");
 			data.put("ConsignmentDateTime", row.getString("data:yd_1_time"));
 			data.put("BusinessTypeCode", row.getString("data:business"));
-			data.put("DespatchActualDateTime", row.getString("data:yd_2_time"));
-			data.put("GoodsReceiptDateTime", row.getString("data:yd_3_time"));
+			data.put("DespatchActualDateTime", row.getString("data:yd_1_time"));
+			data.put("GoodsReceiptDateTime", row.getString("data:yd_2_time"));
 
 			data.put("CountrySubdivisionCode_fh", row.getString("data:from_code"));
 			data.put("CountrySubdivisionCode_sh", row.getString("data:to_code"));
